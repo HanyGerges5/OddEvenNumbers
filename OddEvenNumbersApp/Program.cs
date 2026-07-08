4e9a9dc6364e8cc92efff7a7d1a2b3c25c42abb5
Hany Gerges <gergeshany272@gmail.com>
2026-07-08 19:39:16 +0300

    refector is even and is odd using expression body method

--- OddEvenNumbersApp/Program.cs
+++ OddEvenNumbersApp/Program.cs
@@ -25,29 +25,8 @@ namespace OddevenNumbersApp
             Console.Write($" ]");
             Console.WriteLine();
         }
-        static bool IsEven(int number)
-        {
-            if(number % 2==0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        static bool IsOdd(int number)
-        {
-            if (number % 2 != 0)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+        static bool IsEven(int number) => number % 2 == 0;
+        static bool IsOdd(int number) => !IsEven(number);
     }
 
 
